748d7c20963e4f2f00b371ddeb2892b5b498d944
Ethan Perkins <ethanperkinspro@gmail.com>
2021-09-26 13:00:14 -0600

    finished gitInfo.js

--- gitInfo.js
+++ gitInfo.js
@@ -9,7 +9,7 @@
     It should be a string containing your best definition of what Git is.
 */
 
-//CODE HERE
+let gitDefinition = "a version control system that allows you to go back to previous working models of your code, and also allow you to share it with others allowing for collaboration."
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -17,7 +17,7 @@
     It should be a string containing your best definition of what GitHub is.
 */
 
-//CODE HERE
+let gitHubDefinition = "a website that uses git that allows users to share code with coworkers, and a nice clean site that allows users to utilize git."
 
 //////////////////PROBLEMS 3 - 8////////////////////
 /*
@@ -33,7 +33,10 @@
     following the guidelines above to describe the init command.
 */
 
-//CODE HERE
+const init = {
+    description: "starts the process for creating a git repository. It tells the command line that the next commands are going to be regarding the repo that was just initialized",
+    code: "git init"
+}
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -41,7 +44,11 @@
     following the guidelines above to describe the clone command.
 */
 
-//CODE HERE
+const clone = {
+    description: "a command used to clone an existing repository and create another copy of it",
+    code: "git clone"
+
+}
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -49,7 +56,10 @@
     following the guidelines above to describe the status command.
 */
 
-//CODE HERE
+const status = {
+    description: "a command used to see the current state the repository is in",
+    code: "git status"
+}
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -57,7 +67,10 @@
     following the guidelines above to describe the add command.
 */
 
-//CODE HERE
+const add = {
+    description: "a command used to add new work done to a codebase to your repository.  it tells the command line whatever you're going to commit is what you just added",
+    code: "git add ."
+}
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -65,7 +78,10 @@
     following the guidelines above to describe the commit command.
 */
 
-//CODE HERE
+const commit = {
+    description: "used to add your code to a repository, also requires a commit message to keep things more organized",
+    code: "git commmit -m + commit message in quotes"
+}
 
 
 //////////////////PROBLEM 8////////////////////
@@ -74,4 +90,7 @@
     following the guidelines above to describe the push command.
 */
 
-//CODE HERE
+const push = {
+    description: "used to push your code to the main branch.  Merging your code with the main repository",
+    code: "git push"
+}
